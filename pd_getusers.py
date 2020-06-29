3e1b7b2363c81a3f095e21474e5f6021e5a6ed12
Richard P Chounoune <34277584+rchounoune@users.noreply.github.com>
2020-06-28 21:52:57 -0400

    Create pd_getusers.py

---
+++ pd_getusers.py
@@ -0,0 +1,35 @@
+import requests
+import pandas as pd
+
+# API Key
+API_KEY = ''
+
+
+def list_users():
+
+    # Pagination, 100 users per requests for 320 users
+    for offset in range(0, 320, 100):   
+        url = 'https://api.pagerduty.com/users?limit=100&offset={0}'.format(offset)
+        headers = {
+            'Accept': 'application/vnd.pagerduty+json;version=2',
+            'Authorization': 'Token token={token}'.format(token=API_KEY)
+        }
+        
+        r = requests.get(url, headers=headers)
+        data = r.json()
+        
+        # Empty Dict to hold emails and id numbers
+        users = {}
+        
+        # Looping through json dump to get emails and id numbers
+        for user in (data['users'][0:]):
+            users['email'] = user['email']
+            users['id'] = user['id']
+            
+            # Creating Dataframe with users dictionnary and appending to csv file
+            df = pd.DataFrame([users], columns=users.keys()).drop_duplicates()
+            df.to_csv('pd_users.csv', mode='a', header=False, index=False)
+
+
+if if __name__ == '__main__':
+    list_users()
